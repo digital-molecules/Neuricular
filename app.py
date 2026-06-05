7070d3b701ecee2eb9bcf808edc3b134f383773d
thally <thaliaarvanitidou1@hotmail.com>
2026-06-05 18:42:25 +0100

    Include author caption in the app
    Add caption with author name to the app.

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
+st.caption("By Efthalia Arvanitidou.")
 
 # ── Dark theme CSS ────────────────────────────────────────────────────────────
 st.markdown("""
